7dfb6acc8e6cd987e0af80d6b493e5a3428e7aeb
Abel-Lagonell <alagonell1730@floridapoly.edu>
2022-11-18 19:01:42 -0500

    Formatting but 5 is wrong

--- Reg HW/Lab 8/Lab8.cpp
+++ Reg HW/Lab 8/Lab8.cpp
@@ -14,11 +14,10 @@
 using namespace std;
 
 class Complex{
-private:
+public:
     //Priv variables
     double real=0, img=0;
 
-public: 
     //Constructors
     Complex(double a, double b);
     Complex(double a);
@@ -29,27 +28,25 @@ public:
     double getImaginaryPart() const {return img;} 
 
     //Gives the complex number as a string
-    string toString(){
+    string toString() const {
         if (getImaginaryPart() == 0)
-            return to_string(getRealPart());
-        else{
-            string var = string(to_string(getRealPart()) + ((getImaginaryPart()>0)? " + ":" - ") + to_string(sqrt(pow(getImaginaryPart(),2))) + "i");
-            return var;
-        }
+            return ((getRealPart()>0)? "":"- ") + to_string(std::abs(getRealPart()));
+        else
+            return ((getRealPart()>0)? "":"- ") + to_string(std::abs(getRealPart())) + ((getImaginaryPart()>0)? " + ":" - ") + to_string(std::abs(getImaginaryPart())) + "i";
     }
     
     //normal math functions
-    Complex add(Complex a) const {
+    Complex add(const Complex& a) const {
         double r = getRealPart() + a.getRealPart();
         double i = getImaginaryPart() + a.getImaginaryPart();
         return Complex(r,i);
     }
-    Complex subtract(Complex a) const{
+    Complex subtract(const Complex& a) const{
         double r = getRealPart() - a.getRealPart();
         double i = getImaginaryPart() - a.getImaginaryPart();
         return Complex(r,i);
     }
-    Complex multiply(Complex x) const{
+    Complex multiply(const Complex& x) const{
         double a = getRealPart();
         double b = getImaginaryPart();
         double c = x.getRealPart();
@@ -58,7 +55,7 @@ public:
         double i = b*c + a*d;
         return Complex(r,i);
     }
-    Complex divide(Complex x) const{
+    Complex divide(const Complex& x) const{
         double a = getRealPart();
         double b = getImaginaryPart();
         double c = x.getRealPart();
@@ -78,7 +75,7 @@ public:
     Complex& operator/=(const Complex& a) {return *this = divide(a);}
     
     //Index operator
-    double operator[](int index) {return ((index==0)? getRealPart():getImaginaryPart());}
+    double operator[](int index) {return ((index==0)? getRealPart():((index ==1)? getImaginaryPart():0));}
     
     //addative and subtractive suffix and prefix
     Complex& operator++() {return *this = add(Complex(1));}
@@ -97,26 +94,24 @@ public:
     //Change of sign
     Complex operator+() {return *this;}
     Complex operator-() {return this->multiply(Complex(-1));}
-
-    //in and out operator
-    ostream& operator<<(ostream& out){
-        cout << toString();
-        return out;
-    }
-    istream& operator>>(istream& in){
-        cout << "Enter a real part: ";
-        in >> this->real;
-        cout << "\n Enter a imaginary part: ";
-        in >> this->img;
-    }
-
 };
 
+//in and out operator
+ostream& operator<<(ostream& out, const Complex& a){
+    cout << a.toString();
+    return out;
+}
+istream& operator>>(istream& in, Complex& a){
+    cout << "Enter number a and b for (a + bi): ";
+    cin >> a.real >> a.img;
+    return in;
+}
+
 //normal math operators
-Complex operator+(Complex& a, Complex& b) {return a.add(b);}
-Complex operator-(Complex& a, Complex& b) {return a.subtract(b);}
-Complex operator*(Complex& a, Complex& b) {return a.multiply(b);}
-Complex operator/(Complex& a, Complex& b) {return a.divide(b);}
+Complex operator+(const Complex& a, const Complex& b) {return a.add(b);}
+Complex operator-(const Complex& a, const Complex& b) {return a.subtract(b);}
+Complex operator*(const Complex& a, const Complex& b) {return a.multiply(b);}
+Complex operator/(const Complex& a, const Complex& b) {return a.divide(b);}
 
 //Constructors
 Complex::Complex(double a, double b) {real = a; img = b;}
@@ -124,6 +119,75 @@ Complex::Complex(double a) {real = a; img = 0;}
 Complex::Complex(){real = 0; img = 0;}
 
 int main(){
+    double r,i;
 
+    Complex c0 = Complex();
+    Complex c1, c2, cx1, cx2;
+    
+    cout << 
+        "1. Test constructors: " << endl <<
+        "1.1 Create a complex number c0 with no-arg constructor Complex(): " << endl <<
+        "c0 = " << c0 << ", its the real part Re(c0) = " << c0.getRealPart() << ", its imaginary part Im(c0) = " << c0.getImaginaryPart() << ", its absolute value |c0| = " << c0.abs() << endl <<
+        "1.2 Create a complex number c1 with constructor Complex(a): " << endl;
+    cout << "Enter number a: ";
+    cin >> r;
+    c1 = Complex(r);
+    cout << 
+        "c1 = " << c1 << ", its the real part Re(c1) = " << c1.getRealPart() << ", its imaginary part Im(c1) = " << c1.getImaginaryPart() << ", its absolute value |c1| = " << c1.abs() << endl <<
+        "1.3 Create a complex number c2 with constructor Complex(a,b): " << endl;
+    cout << "Enter number a b: ";
+    cin >> r >> i;
+    c2 = Complex(r,i);
+    cout << 
+        "c2 = " << c2 << ", its the real part Re(c2) = " << c2.getRealPart() << ", its imaginary part Im(c2) = " << c2.getImaginaryPart() << ", its absolute value |c2| = " << c2.abs() << endl << endl <<
+        "2. Test overloaded stream insertion operator >> and extraction operator <<: " << endl << 
+        "Enter two complex numbers following the prompts:" << endl <<
+        "Enter the first complex number cx1 with (cin >> cx1):";
+    cin >> cx1;
+    cout << "\'cout << cx1\' prints " << cx1 << endl;
+    cout << "Enter the second complex number cx2 with (cin >> cx2):";
+    cin >> cx2;
+    cout << 
+        "\'cout << cx2\' prints " << cx2 << endl << endl <<
+        "3. Test overloaded arithmetic operators +,-,*,/:" << endl <<
+        "(" << cx1 << ") + (" << cx2 << " = " << (cx1 + cx2) << endl <<
+        "(" << cx1 << ") - (" << cx2 << " = " << (cx1 - cx2) << endl <<
+        "(" << cx1 << ") * (" << cx2 << " = " << (cx1 * cx2) << endl <<
+        "(" << cx1 << ") / (" << cx2 << " = " << (cx1 / cx2) << endl << endl <<
+        "4. Test overloaded augmented arithmetic operators +=,-=,*=,/=:" << endl << 
+        "cx1 = " << cx1 << "; cx2 = " << cx2 << endl;
+    cx2 += cx1;
+    cout << "( cx2 += cx1 ); cx2 = " << cx2 << endl;
+    cx2 -= cx1;
+    cout << "( cx2 -= cx1 ); cx2 = " << cx2 << endl;
+    cx2 *= cx1;
+    cout << "( cx2 *= cx1 ); cx2 = " << cx2 << endl;
+    cx2 /= cx1;
+    cout << 
+        "( cx2 /= cx1 ); cx2 = " << cx2 << endl << endl <<
+        "5. Test overloaded unary operators:" << endl <<
+        "5.1 Test overloaded sign operators +,-" << endl <<
+        "cx1 = " << cx1 << endl <<
+        "+cx1 = " << +cx1 << endl <<
+        "-cx1 = " << -cx1 << endl <<
+        "5.2 Test overloaded prefixes ++, --:" << endl <<
+        "cx1 = " << cx1 << endl <<
+        "++cx1; cx1 = " << ++cx1 << endl <<
+        "--cx1; cx1 = " << --cx1 << endl <<
+        /*"5.2 Test overloaded postfixes ++,--:" << endl <<
+        "cx1 = " << cx1 << endl <<
+        "\'cout << cx1++;\' prints " << cx1++ << endl <<
+        "\'cout << cx1++;\' prints " << cx1-- << endl <<
+        "cx1 = " << cx1 << endl << endl <<*/
+        "6. Test overloaded subscript operator []:" << endl <<
+        "cx1 = " << cx1 << endl <<
+        "cx1[0] = " << cx1[0] << endl <<
+        "cx1[1] = " << cx1[1] << endl <<
+        "(cx2[0] == cx2.getRealPart()) = " << (cx2[0] == cx2.getRealPart()) << endl <<
+        "(cx2[1] == cx2.getImaginaryPart()) = " << (cx2[1] == cx2.getImaginaryPart()) << endl << endl <<
+        "7. Test implicit conversion from double to Complex:" << endl <<
+        "cx2 = " << cx2 << endl <<
+        "3 + cx2 = " << 3.0 + cx2 << endl <<
+        "3.4 + cx2 = " << 3.4 + cx2 << endl;
     return 0;
 }
